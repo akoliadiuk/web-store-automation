628c72f7c0406333d4a132834a3536cfa2b4d847
akoliadiuk <akoliadiuk@gmail.com>
2023-12-15 12:00:33 +0200

    Added linter, pre-commit hooks

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,9 @@
 import { defineConfig, devices } from '@playwright/test';
 
+const viewport = {
+  width: 1920,
+  height: 1080,
+};
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -10,7 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './test/specs',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -21,13 +25,15 @@ export default defineConfig({
   workers: process.env.CI ? 1 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [process.env.CI ? ['list'] : ['list', 'html']],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  /* Shared settings for all the projects below.
+  See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://www.ebay.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: false,
   },
 
   /* Configure projects for major browsers */
@@ -36,7 +42,8 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        screenshot: true
+        viewport,
+        screenshot: true,
       },
     },
 
@@ -44,7 +51,8 @@ export default defineConfig({
       name: 'firefox',
       use: {
         ...devices['Desktop Firefox'],
-        screenshot: true
+        viewport,
+        screenshot: true,
       },
     },
 
@@ -52,7 +60,8 @@ export default defineConfig({
       name: 'webkit',
       use: {
         ...devices['Desktop Safari'],
-        screenshot: true
+        viewport,
+        screenshot: true,
       },
     },
   ],
